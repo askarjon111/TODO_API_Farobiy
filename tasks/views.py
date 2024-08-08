f56c013fcea7e22df876838961b5b4d383d8c0ae
ASKAR <askarjon.abdullayev@gmail.com>
2024-08-08 20:07:47 +0500

    Task ModelViewSet

--- tasks/views.py
+++ tasks/views.py
@@ -5,17 +5,22 @@ from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
 
-class TaskViewSet(viewsets.ViewSet):
-    def list(self, request):
-        tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data, 200)
+# class TaskViewSet(viewsets.ViewSet):
+#     def list(self, request):
+#         tasks = Task.objects.all()
+#         serializer = TaskSerializer(tasks, many=True)
+#         return Response(serializer.data, 200)
 
-    def create(self, request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            return Response({'error': serializer.errors}, 400)
+#     def create(self, request):
+#         serializer = TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#         else:
+#             return Response({'error': serializer.errors}, 400)
 
-        return Response(serializer.data, 201)
+#         return Response(serializer.data, 201)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
